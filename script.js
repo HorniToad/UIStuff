1bf9b67f9fcaf3331a63b855c6e00380f278701e
HorniToad <maia.ackley@proton.me>
2025-12-05 23:32:26 -0700

    Added some buttons to control the current scene to the left stat container and made some changes to the inner scroll bar

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(function gameStateHandler () {
     let parent = $(scene).parent([".sceneContainerHidden"])
     parent[0].style.display = "grid"
     let buildingTemplate = {floorSlots: [], buildingSlots: [], slots: 20, floors: 5, buildFocus: false, buildTypeFocus: false, buildItemFocus: false, buildingSceneFocus: false}
-    let personnelTemplate = {patients: [testDummy, testDummy2, testDummy, testDummy, testDummy, testDummy, testDummy, testDummy], employees: [], customers: []};
+    let personnelTemplate = {patients: [testDummy, testDummy2, testDummy, testDummy, testDummy, testDummy, testDummy, testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy,testDummy], employees: [], customers: []};
 
     gameState = {tickState: false, tickRate: false, completeTick: 10, currentTick: 0, date: false, currentDay: 1, cash: 500, patientCap: false, conditionFocus: false, charGen: false, personnel: personnelTemplate, buildings: buildingTemplate, currentScene: {current: scene, prior: false, parent: parent[0] } }
     console.log(gameState)
